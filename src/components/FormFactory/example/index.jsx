0380b75bc2ac099f71e464294a5d9dac2399026d
pafev <paulofvilarimda@gmail.com>
2023-03-22 17:58:48 -0300

    component as a factory

--- src/components/FormFactory/example/index.jsx
+++ src/components/FormFactory/example/index.jsx
@@ -1,41 +1,40 @@
+// import { useState } from "react";
 import { FormFactory } from "../index";
 
-export function UserForm() {
-  const userSchema = [
-    {
-      field: "name",
-      placeholder: "name",
-      type: "text", // input type, could be image, etc
-      required: true,
-    },
-    {
-      field: "age",
-      placeholder: "age",
-      type: "number",
-      required: false,
-    },
-    {
-      field: "email",
-      placeholder: "email",
-      type: "email",
-      required: true,
-    },
-    {
-      field: "password",
-      placeholder: "password",
-      type: "password",
-      required: true,
-      customValidation: (formInfo) => formInfo.password.length > 6,
-    },
-    {
-      field: "passwordConfirmation",
-      placeholder: "repeat password",
-      type: "password",
-      required: true,
-      customValidation: (formInfo) =>
-        formInfo.password === formInfo.passwordConfirmation,
-    },
-  ];
+const userSchema = [
+  {
+    field: "name",
+    placeholder: "name",
+    type: "text", // input type, could be image, etc
+    required: true,
+  },
+  {
+    field: "age",
+    placeholder: "age",
+    type: "number",
+    required: false,
+  },
+  {
+    field: "email",
+    placeholder: "email",
+    type: "email",
+    required: true,
+  },
+  {
+    field: "password",
+    placeholder: "password",
+    type: "password",
+    required: true,
+    customValidation: (formInfo) => formInfo.password.length > 6,
+  },
+  {
+    field: "passwordConfirmation",
+    placeholder: "repeat password",
+    type: "password",
+    required: true,
+    customValidation: (formInfo) =>
+      formInfo.password === formInfo.passwordConfirmation,
+  },
+];
 
-  return <FormFactory config={userSchema} />;
-}
+export const UserForm = FormFactory(userSchema);
--- src/components/FormFactory/index.jsx
+++ src/components/FormFactory/index.jsx
@@ -1,57 +1,58 @@
 import { useState } from "react";
 
-export function FormFactory({ config }) {
-  const [formInfo, setFormInfo] = useState({});
+export function FormFactory(schema) {
+  const handleOnSubmit = (event, formInfo, ValidSubmit, InvalidSubmit) => {
+    event.preventDefault();
 
-  const handleOnChangeFormInfo = (event, key) => {
-    setFormInfo({ ...formInfo, [key]: event.target.value });
-  };
+    let valid = true;
 
-  const handleOnSubmit = () => {
-    // eslint-disable-next-line no-console
-    console.log(formInfo);
-  };
-
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-
-        let valid = true;
-
-        config.forEach((item) => {
-          if (item.customValidation) {
-            if (!item.customValidation(formInfo)) {
-              valid = false;
-            }
-          }
-        });
-
-        if (valid) {
-          handleOnSubmit();
-        } else {
-          // eslint-disable-next-line no-alert
-          alert("erro de validação");
+    schema.forEach((item) => {
+      if (item.customValidation) {
+        if (!item.customValidation(formInfo)) {
+          valid = false;
         }
-      }}
-    >
-      <h1>Formulário</h1>
-      {config &&
-        config.map((item) => {
-          const { field } = item;
+      }
+    });
 
-          return (
-            <input
-              key={field}
-              type={item.type}
-              placeholder={item.placeholder}
-              required={item.required ? "required" : ""}
-              value={formInfo[field] ? formInfo[field] : ""}
-              onChange={(event) => handleOnChangeFormInfo(event, field)}
-            />
-          );
-        })}
-      <button type="submit">Enviar</button>
-    </form>
-  );
+    if (valid) {
+      ValidSubmit(formInfo);
+    } else {
+      InvalidSubmit(formInfo);
+    }
+  };
+
+  // eslint-disable-next-line func-names
+  return function ({ ValidSubmit, InvalidSubmit }) {
+    const [formInfo, setFormInfo] = useState({});
+
+    const handleOnChangeFormInfo = (event, key) => {
+      setFormInfo({ ...formInfo, [key]: event.target.value });
+    };
+
+    return (
+      <form
+        onSubmit={(event) =>
+          handleOnSubmit(event, formInfo, ValidSubmit, InvalidSubmit)
+        }
+      >
+        {/* <h1>Formulário</h1> */}
+        {schema &&
+          schema.map((item) => {
+            const { field } = item;
+
+            return (
+              <input
+                key={field}
+                type={item.type}
+                placeholder={item.placeholder}
+                required={item.required ? "required" : ""}
+                value={formInfo[field] ? formInfo[field] : ""}
+                onChange={(event) => handleOnChangeFormInfo(event, field)}
+              />
+            );
+          })}
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  };
 }
