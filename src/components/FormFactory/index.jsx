4a6e5161d15527c1b4c30cc311a32dd9e8dc623b
pafev <paulofvilarimda@gmail.com>
2023-03-22 05:15:22 -0300

    applying manipulable formInfo in formFactory

--- src/components/FormFactory/index.jsx
+++ src/components/FormFactory/index.jsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
+
 export function FormFactory({ config }) {
+  const [formInfo, setFormInfo] = useState({});
+
+  const handleOnChangeFormInfo = (event, key) => {
+    setFormInfo({ ...formInfo, [key]: event.target.value });
+  };
+
+  const handleOnSubmit = () => {
+    // eslint-disable-next-line no-console
+    console.log(formInfo);
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        let valid = true;
+
+        config.forEach((item) => {
+          if (item.customValidation) {
+            if (!item.customValidation(formInfo)) {
+              valid = false;
+            }
+          }
+        });
+
+        if (valid) {
+          handleOnSubmit();
+        } else {
+          // eslint-disable-next-line no-alert
+          alert("erro de validação");
+        }
+      }}
+    >
       <h1>Formulário</h1>
       {config &&
         config.map((item) => {
-          // eslint-disable-next-line no-console
-          console.log(item);
+          const { field } = item;
 
           return (
             <input
-              key={item.field}
+              key={field}
               type={item.type}
               placeholder={item.placeholder}
               required={item.required ? "required" : ""}
+              value={formInfo[field] ? formInfo[field] : ""}
+              onChange={(event) => handleOnChangeFormInfo(event, field)}
             />
           );
         })}
