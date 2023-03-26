ca128efb92f9c94a482e0b56b3395afbff7e5b8d
pafev <paulofvilarimda@gmail.com>
2023-03-26 04:24:31 -0300

    example page 1: UserForm call

--- src/components/FormFactory/example/index.jsx
+++ src/components/FormFactory/example/index.jsx
@@ -1,7 +1,8 @@
 // import { useState } from "react";
+import styled from "styled-components";
 import { FormFactory } from "../index";
 
-const userSchema = [
+const registerSchema = [
   {
     field: "name",
     placeholder: "name",
@@ -9,16 +10,16 @@ const userSchema = [
     required: true,
   },
   {
-    field: "age",
-    placeholder: "age",
-    type: "number",
-    required: false,
-  },
-  {
     field: "email",
     placeholder: "email",
     type: "email",
     required: true,
+  },
+  {
+    field: "age",
+    placeholder: "age",
+    type: "number",
+    required: false,
   },
   {
     field: "password",
@@ -37,4 +38,45 @@ const userSchema = [
   },
 ];
 
-export const UserForm = FormFactory(userSchema);
+const FormStyled = styled.form`
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    > input {
+      width: 10rem;
+      height: 2rem;
+      border: none;
+      border-radius: 5px;
+      outline: none;
+      padding: 0.5rem;
+    }
+    > input + input {
+      margin-top: 0.5rem;
+    }
+    > button {
+      width: 4rem;
+      height: 2rem;
+      margin: 1rem;
+    }
+  }
+`;
+
+export function FormFactoryExample() {
+  const RegisterForm = FormFactory(registerSchema);
+
+  return (
+    <>
+      <h1>Registre-se</h1>
+      <FormStyled>
+        <RegisterForm
+          // eslint-disable-next-line no-console
+          onValidSubmit={(formInfo) => console.log(formInfo)}
+          // eslint-disable-next-line no-alert
+          onInvalidSubmit={() => alert("não deu bom")}
+          buttonValue="Registrar"
+        />
+      </FormStyled>
+    </>
+  );
+}
