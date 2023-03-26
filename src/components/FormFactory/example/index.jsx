074e5b8c8f46e1a5db479555e339030e590355c4
pafev <paulofvilarimda@gmail.com>
2023-03-26 05:06:10 -0300

    example 1: login and register sections alternating

--- src/components/FormFactory/example/index.jsx
+++ src/components/FormFactory/example/index.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
-import styled from "styled-components";
+import { useState } from "react";
 import { FormFactory } from "../index";
+import { FormStyled, PageLayout } from "./styles";
 
 const registerSchema = [
   {
@@ -38,45 +39,61 @@ const registerSchema = [
   },
 ];
 
-const FormStyled = styled.form`
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    > input {
-      width: 10rem;
-      height: 2rem;
-      border: none;
-      border-radius: 5px;
-      outline: none;
-      padding: 0.5rem;
-    }
-    > input + input {
-      margin-top: 0.5rem;
-    }
-    > button {
-      width: 4rem;
-      height: 2rem;
-      margin: 1rem;
-    }
-  }
-`;
+const loginSchema = [
+  {
+    field: "email",
+    placeholder: "email",
+    type: "email",
+    required: true,
+  },
+  {
+    field: "password",
+    placeholder: "password",
+    type: "password",
+    required: true,
+    customValidation: (formInfo) => formInfo.password.length > 6,
+  },
+];
 
 export function FormFactoryExample() {
   const RegisterForm = FormFactory(registerSchema);
+  const LoginForm = FormFactory(loginSchema);
+
+  const [validSection, setValidSection] = useState("register");
 
   return (
-    <>
-      <h1>Registre-se</h1>
-      <FormStyled>
-        <RegisterForm
-          // eslint-disable-next-line no-console
-          onValidSubmit={(formInfo) => console.log(formInfo)}
-          // eslint-disable-next-line no-alert
-          onInvalidSubmit={() => alert("não deu bom")}
-          buttonValue="Registrar"
-        />
-      </FormStyled>
-    </>
+    <PageLayout>
+      <section className={validSection === "register" ? "" : "invalidSection"}>
+        <h1>Registre-se</h1>
+        <FormStyled>
+          <RegisterForm
+            // eslint-disable-next-line no-console
+            onValidSubmit={(formInfo) => console.log(formInfo)}
+            // eslint-disable-next-line no-alert
+            onInvalidSubmit={() => alert("não deu bom")}
+            buttonValue="Registrar"
+          />
+        </FormStyled>
+        <button type="button" onClick={() => setValidSection("enter")}>
+          Já tenho registro
+        </button>
+      </section>
+
+      <section className={validSection === "enter" ? "" : "invalidSection"}>
+        <h1>Entrar</h1>
+        <FormStyled>
+          <LoginForm
+            // eslint-disable-next-line no-console
+            onValidSubmit={(formInfo) => console.log(formInfo)}
+            // eslint-disable-next-line no-alert
+            onInvalidSubmit={() => alert("não deu bom")}
+            buttonValue="Entrar"
+          />
+        </FormStyled>
+        <button type="button" onClick={() => setValidSection("register")}>
+          Ainda não sou registrado
+        </button>
+      </section>
+    </PageLayout>
   );
 }
