70bb2df046903b47ad64e603435992f9f456c12d
pafev <paulofvilarimda@gmail.com>
2023-03-26 16:21:29 -0300

    finalizing and commenting on example 1

--- src/components/FormFactory/example/index.jsx
+++ src/components/FormFactory/example/index.jsx
@@ -1,4 +1,8 @@
-// import { useState } from "react";
+// A proposta deste exemplo é uma página de cadastro e login de usuário
+//  Você tem a opção de registrar um novo usuário e, caso já tenha registrado algum, você
+//  pode ir para a página de login e logar com os devidos email e senha do usuário cadastrado
+// obs.: a lista de usuários reseta ao reiniciar a página
+
 import { useState } from "react";
 import { FormFactory } from "../index";
 import { FormStyled, PageLayout } from "./styles";
@@ -15,6 +19,13 @@ const registerSchema = [
     placeholder: "email",
     type: "email",
     required: true,
+    customValidation: (formInfo, usersData) => {
+      // verifica se o email já está cadastrado
+      const user = usersData.find(
+        (element) => element.email === formInfo.email
+      );
+      return !user;
+    },
   },
   {
     field: "age",
@@ -45,24 +56,29 @@ const loginSchema = [
     placeholder: "email",
     type: "email",
     required: true,
-    customValidation: (formInfo, userData) => formInfo.email === userData.email,
   },
   {
     field: "password",
     placeholder: "password",
     type: "password",
     required: true,
-    customValidation: (formInfo, userData) =>
-      formInfo.password === userData.password,
+    customValidation: (formInfo, usersData) => {
+      // verifica se a senha está correta para o email preenchido
+      const user = usersData.find(
+        (element) => element.email === formInfo.email
+      );
+      return user ? user.password === formInfo.password : false;
+    },
   },
 ];
 
 export function FormFactoryExample() {
-  const [userData, setUserData] = useState({});
+  const [usersData, setUsersData] = useState([]);
 
-  const RegisterForm = FormFactory(registerSchema);
-  const LoginForm = FormFactory(loginSchema, userData);
+  const RegisterForm = FormFactory(registerSchema, usersData);
+  const LoginForm = FormFactory(loginSchema, usersData);
 
+  // estado para definir qual sessão (register ou login) será exibida na página
   const [validSection, setValidSection] = useState("register");
 
   return (
@@ -72,13 +88,13 @@ export function FormFactoryExample() {
         <FormStyled>
           <RegisterForm
             onValidSubmit={(formInfo) => {
-              setUserData(formInfo);
+              setUsersData([...usersData, formInfo]);
               // eslint-disable-next-line no-alert
               alert("registrado com sucesso");
             }}
             // eslint-disable-next-line no-alert
             onInvalidSubmit={() => alert("não deu bom")}
-            buttonValue="Registrar"
+            buttonContent="Registrar"
           />
         </FormStyled>
         <button type="button" onClick={() => setValidSection("enter")}>
@@ -94,7 +110,7 @@ export function FormFactoryExample() {
             onValidSubmit={() => alert("logado com sucesso")}
             // eslint-disable-next-line no-alert
             onInvalidSubmit={() => alert("não deu bom")}
-            buttonValue="Entrar"
+            buttonContent="Entrar"
           />
         </FormStyled>
         <button type="button" onClick={() => setValidSection("register")}>
