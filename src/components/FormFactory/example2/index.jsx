12a747d647b66c77714fe97ddef4e28da1c311a6
pafev <paulofvilarimda@gmail.com>
2023-03-31 12:53:09 -0300

    completely changing example 2

--- src/components/FormFactory/example2/index.jsx
+++ src/components/FormFactory/example2/index.jsx
@@ -1,50 +1,95 @@
+import { useState } from "react";
 import { FormFactory } from "..";
 
-const userSchema = [
+const addFormSchema = [
   {
-    field: "name",
+    field: "newField",
+    placeholder: "field",
     required: true,
-    type: "text",
-    placeholder: "nome de usuário",
+    label: "Campo que representa o input",
   },
   {
-    field: "age",
+    field: "newPlaceholder",
+    placeholder: "placeholder",
     required: false,
-    type: "number",
-    placeholder: "idade",
+    label: "Placeholder do input",
   },
   {
-    field: "profilePicture",
+    field: "newRequired",
+    placeholder: "required",
     required: false,
-    type: "file",
-    label: "Foto de perfil",
+    label: "Este input é obrigatório? (y/n)",
+    customValidation: (formInfo) => {
+      if (formInfo.newRequired) {
+        return ["y", "Y", "yes", "Yes", "n", "N", "no", "No"].includes(
+          formInfo.newRequired
+        );
+      }
+      return true;
+    },
   },
   {
-    field: "favAnime",
-    required: true,
-    type: "text",
-    placeholder: "anime favorito",
-  },
-  {
-    field: "favManga",
+    field: "newType",
+    placeholder: "type",
     required: false,
-    type: "text",
-    placeholder: "manga/manhwa favorito",
+    label: "Tipo do input",
+    customValidation: (formInfo) => {
+      if (formInfo.newType) {
+        return [
+          "text",
+          "number",
+          "file",
+          "email",
+          "password",
+          "image",
+        ].includes(formInfo.newType);
+      }
+      return true;
+    },
   },
   {
-    field: "submit",
-    type: "image",
-    alt: "Salvar",
+    field: "newLabel",
+    placeholder: "label",
+    required: false,
+    label: "Label que acompanhará o input",
+  },
+  {
+    field: "newAlt",
+    placeholder: "alt",
+    required: false,
+    label: "É um input image? Digite seu alt",
   },
 ];
 
 export function FormFactoryExample2() {
-  const UserForm = FormFactory(userSchema);
+  const [formSchema, setFormSchema] = useState([]);
+  const UserForm = FormFactory(formSchema);
+  const AddFormForm = FormFactory(addFormSchema);
 
   return (
     <>
-      <h1>Seu perfil e suas obras favoritas</h1>
+      <h1>Seu formulário</h1>
       <UserForm />
+      <h1>Adicione um input ao seu formulário</h1>
+      <AddFormForm
+        buttonContent="Adicionar"
+        // eslint-disable-next-line no-alert
+        onValidSubmit={(formInfo) =>
+          setFormSchema([
+            ...formSchema,
+            {
+              field: formInfo.newField,
+              placeholder: formInfo.newPlaceholder,
+              required: ["y", "Y", "yes", "Yes"].includes(formInfo.newRequired),
+              type: formInfo.newType,
+              label: formInfo.newLabel,
+              alt: formInfo.newAlt,
+            },
+          ])
+        }
+        // eslint-disable-next-line no-alert
+        onInvalidSubmit={() => alert("não deu bom")}
+      />
     </>
   );
 }
