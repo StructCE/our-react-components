9f8b5f34b02831c5bfa02691a0f3bbf1b04e5993
jessica8melo <jessica8melo@gmail.com>
2023-04-04 15:25:20 -0300

    ✨ feature Carrousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,8 +1,40 @@
-export function Carousel({ children, id, ...props }) {
+// Comentar como usar
+import { useState } from "react";
+import { ArrowLeft, ArrowRight } from "./svgs";
+
+export function Carousel({ schema, style }) {
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+  function changePosition(i) {
+    if (i > schema.length) {
+      return;
+    }
+
+    if (i < 0) {
+      return;
+    }
+
+    setCurrentPosition(i);
+  }
+
+  const nextIndex = () => changePosition(currentPosition + 1);
+
+  const prevIndex = () => changePosition(currentPosition - 1);
+
   return (
-    <>
-      <input id={id} {...props} type="carousel" />
-      <label htmlFor="{id}">{children}</label>
-    </>
+    <div className="Container" style={{ ...style }}>
+      <ArrowLeft className="left-arrow" onClick={prevIndex} />
+      <ArrowRight className="right-arrow" onClick={nextIndex} />
+      {schema.map((item, index) => (
+        <div
+          className={index === currentPosition ? "item active" : "item"}
+          key={item.index}
+        >
+          {index === currentPosition && (
+            <img src={item.image} alt={item.title} />
+          )}
+        </div>
+      ))}
+    </div>
   );
 }
