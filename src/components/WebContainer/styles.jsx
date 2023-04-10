cb7bf153865bdd193a71021d061f4f4c78458e8c
pafev <paulofvilarimda@gmail.com>
2023-04-10 17:29:44 -0300

    adjusting styles codelayout

--- src/components/WebContainer/styles.jsx
+++ src/components/WebContainer/styles.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const CodeLayout = styled.section`
-  background-color: white;
   box-shadow: rgba(0, 0, 0, 0.3) -2px 8px 80px -6px;
   min-height: 8rem;
   h1 {
@@ -9,8 +8,5 @@ export const CodeLayout = styled.section`
     border-bottom: 1px solid rgba(0, 0, 0, 0.5);
     width: 60%;
     padding-left: 0.4rem;
-  }
-  pre {
-    margin-left: 1rem;
   }
 `;
