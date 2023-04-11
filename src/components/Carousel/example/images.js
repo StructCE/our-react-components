237139e5afd8a80e7160f56c262a0083ec00e0a4
jessica8melo <jessica8melo@gmail.com>
2023-04-10 22:17:54 -0300

    ⚡️ adaptando as imagens de exemplo

---
+++ src/components/Carousel/example/images.js
@@ -0,0 +1,14 @@
+export const images = [
+  {
+    url: "./Images/Cavalinho1.jpg",
+    alt: "Cavalinho 1",
+  },
+  {
+    url: "./Images/Cavalinho2.jpeg",
+    alt: "Cavalinho 2",
+  },
+  {
+    url: "./Images/Cavalinho3.png",
+    alt: "Cavalinho 3",
+  },
+];
