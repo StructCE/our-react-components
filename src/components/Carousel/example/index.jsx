f78bcccb3cb065e65dcefafbc82c1dd1c80a4230
jessica8melo <jessica8melo@gmail.com>
2023-04-10 22:21:13 -0300

    ⚡️ atualização do exemplo

--- src/components/Carousel/example/index.jsx
+++ src/components/Carousel/example/index.jsx
@@ -5,7 +5,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Carousel } from "..";
-import { data } from "./schema";
+import { images } from "./images";
 
 const PageLayout = styled.section`
   height: 100vh;
@@ -18,39 +18,10 @@ const PageLayout = styled.section`
   align-items: center;
 `;
 
-const Container = styled.div`
-  width: 200px;
-  display: flex;
-
-  .right-arrow {
-    position: absolute;
-    top: 50%;
-    right: 45%;
-  }
-
-  .left-arrow {
-    position: absolute;
-    top: 50%;
-    left: 45%;
-  }
-
-  .button {
-    border-radius: 50%;
-    border: 3px solid #f1f1f1;
-    margin: 0 5px;
-    background: #f1f1f1;
-  }
-  .button.active {
-    background: rgb(32, 32, 32);
-  }
-`;
-
 export function CarouselExample() {
   return (
     <PageLayout>
-      <Container>
-        <Carousel schema={data} />
-      </Container>
+      <Carousel images={images} />
     </PageLayout>
   );
 }
