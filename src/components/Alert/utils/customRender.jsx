2c52d746c5ee068eea4a543ad6f24307dcfc4438
pafev <paulofvilarimda@gmail.com>
2023-05-03 10:56:53 -0300

    removing div alert in index.html

--- src/components/Alert/utils/customRender.jsx
+++ src/components/Alert/utils/customRender.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elemento = document.createElement("div");
+
 export async function customRender(children) {
-  const alertRoot = ReactDOM.createRoot(document.getElementById("alert"));
+  const alertRoot = ReactDOM.createRoot(elemento);
   alertRoot.render(<React.StrictMode>{children}</React.StrictMode>);
 }
