af7664e434c14bc328600b38b3d67e188e0d3307
pafev <paulofvilarimda@gmail.com>
2023-05-05 15:41:26 -0300

    adjusting routes in alert examples

--- src/components/Alert/routes/index.jsx
+++ src/components/Alert/routes/index.jsx
@@ -1,13 +1,15 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Route, Routes } from "react-router-dom";
+/* eslint-disable import/no-extraneous-dependencies */
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { AlertExample } from "../example";
 import { AlertExample2 } from "../example2";
 
 export function AlertRoutes() {
   return (
-    <Routes>
-      <Route Component={AlertExample} path="/alertexample" />
-      <Route Component={AlertExample2} path="/alertexample2" />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/alertexample" element={<AlertExample />} />
+        <Route path="/alertexample2" element={<AlertExample2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
