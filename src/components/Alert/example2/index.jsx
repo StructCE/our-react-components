58a3d7c18a4d0e059d488bef87edbc2de594b733
pafev <paulofvilarimda@gmail.com>
2023-05-16 14:00:58 -0300

    styles for example 2

--- src/components/Alert/example2/index.jsx
+++ src/components/Alert/example2/index.jsx
@@ -1,68 +1,96 @@
 import { useState } from "react";
-import { Alert, AlertCall } from "..";
+import { Alert, AlertCall } from "./stylizedAlerts";
 
 export function AlertExample2() {
   const [isManagingAlertCall, setIsManagingAlertCall] = useState(true);
-  const [responseForAlertCall, setResponseForAlertCall] = useState(false);
-  const [responseForAlertComponent, setResponseForAlertComponent] =
-    useState(false);
+  const [response, setResponse] = useState(false);
 
   const handleClick = async () => {
-    setResponseForAlertCall(await AlertCall());
+    setResponse(await AlertCall({ title: "Tem certeza?" }));
   };
 
-  return (
-    <>
-      <h1>Escolha seu alert</h1>
+  const linesFunctionCode = [
+    "<button",
+    " onClick={async () => setResponse(await AlertCall())}",
+    ">",
+    "  Emitir status",
+    "</button>",
+  ];
 
-      <section>
+  const linesComponentCode = [
+    "<Alert",
+    " onConfirm={() => setResponse(true)",
+    " onCancel={() => setResponse(false)}}",
+    ">",
+    "  <button>",
+    "    Emitir status",
+    "  </button>",
+    "</Alert>",
+  ];
+
+  return (
+    <div className="bg-slate-800 h-screen flex justify-center items-center flex-col">
+      <section className="bg-gray-900 rounded px-5 py-4 shadow-md text-white border-b-4 border-cyan-500">
         <div>
-          <button type="button" onClick={() => setIsManagingAlertCall(true)}>
-            Função alert
+          <button
+            type="button"
+            onClick={() => setIsManagingAlertCall(true)}
+            className="px-4 py-2 mb-4 mr-2 w-32 justify-center hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm inline-flex dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+          >
+            Função Alert
           </button>
-          <button type="button" onClick={() => setIsManagingAlertCall(false)}>
+          <button
+            type="button"
+            onClick={() => setIsManagingAlertCall(false)}
+            className="px-4 py-2 mb-4 ml-2 w-44 justify-center hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm inline-flex dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+          >
             Componente alert
           </button>
         </div>
 
         {isManagingAlertCall ? (
-          <>
-            <h2>
-              Clique no botão para chamar a função Alert e o status reagir
-            </h2>
-
-            <button type="button" onClick={handleClick}>
+          <div className="flex flex-col text-sm font-medium">
+            <button
+              type="button"
+              onClick={handleClick}
+              className="px-4 py-2 mt-4 mb-6 w-32 shadow-md shadow-cyan-800 justify-center hover:bg-cyan-100 border border-cyan-400 font-medium rounded-lg text-sm inline-flex dark:bg-cyan-800 dark:border-cyan-700 dark:text-white dark:hover:bg-cyan-700"
+            >
               Emitir status
             </button>
 
-            {responseForAlertCall ? (
-              <span>Confirmado</span>
-            ) : (
-              <span>Cancelado</span>
-            )}
-          </>
-        ) : (
-          <>
-            <h2>
-              Clique no botão para dar display no componente Alert e o status
-              reagir
-            </h2>
+            {response ? <span>Confirmado</span> : <span>Cancelado</span>}
 
+            <pre className="mb-4 mt-6 bg-gray-950 p-2 rounded-md border-2 border-cyan-600">
+              {linesFunctionCode.join("\n")}
+            </pre>
+          </div>
+        ) : (
+          <div className="flex flex-col text-sm font-medium">
             <Alert
-              onConfirm={() => setResponseForAlertComponent(true)}
-              onCancel={() => setResponseForAlertComponent(false)}
+              onConfirm={() => setResponse(true)}
+              onCancel={() => setResponse(false)}
+              title="Tem certeza?"
             >
-              <button type="button">Emitir status</button>
+              <button
+                type="button"
+                className="my-4 border-b-2 border-cyan-500 w-32 py-2 bg-black rounded"
+              >
+                Emitir status
+              </button>
             </Alert>
 
-            {responseForAlertComponent ? (
-              <span>Confirmado</span>
+            {response ? (
+              <span>Response = Confirmado</span>
             ) : (
               <span>Cancelado</span>
             )}
-          </>
+
+            <pre className="mb-4 mt-6 bg-gray-950 p-2 rounded-md border-2 border-cyan-600">
+              {linesComponentCode.join("\n")}
+            </pre>
+          </div>
         )}
       </section>
-    </>
+    </div>
   );
 }
---
+++ src/components/Alert/example2/stylizedAlerts.jsx
@@ -0,0 +1,80 @@
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import { customRender } from "../utils/customRender";
+
+function Alert({
+  title,
+  content,
+  cancelText,
+  onCancel,
+  confirmText,
+  onConfirm,
+  children,
+  conditionToOpen = true,
+  defaultOpen,
+}) {
+  return (
+    <AlertDialog.Root defaultOpen={defaultOpen}>
+      {conditionToOpen ? (
+        <AlertDialog.Trigger asChild>{children}</AlertDialog.Trigger>
+      ) : (
+        children
+      )}
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay className="fixed inset-0 bg-black/60" />
+        <AlertDialog.Content className="fixed inset-0 flex justify-center items-center">
+          <div className="relative flex flex-col items-center p-2 text-md text-white/80 font-medium rounded-lg bg-gradient-to-br from-gray-950 to-gray-900 border-b-2 border-cyan-600">
+            <h1 className="mt-2 mb-0 pb-4 px-6 text-xl border-b-2 border-white">
+              {title}
+            </h1>
+            <p className="font-normal">{content}</p>
+            <div className="mx-auto mt-2">
+              <AlertDialog.Cancel asChild>
+                <button
+                  type="button"
+                  onClick={onCancel}
+                  className="mx-8 my-4 hover:text-cyan-600 transition-all ease-linear px-2"
+                >
+                  {cancelText || "cancelar"}
+                </button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Action asChild>
+                <button
+                  type="button"
+                  onClick={onConfirm}
+                  className="mx-8 my-4 hover:text-cyan-600 transition-all ease-linear px-2"
+                >
+                  {confirmText || "confirmar"}
+                </button>
+              </AlertDialog.Action>
+            </div>
+          </div>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
+  );
+}
+
+async function AlertCall(attributes) {
+  let value = null;
+  const operation = new Promise((resolve) => {
+    customRender(
+      <Alert
+        defaultOpen
+        onCancel={() => {
+          resolve(false);
+        }}
+        onConfirm={() => {
+          resolve(true);
+        }}
+        {...attributes}
+      />
+    );
+  });
+  await operation.then((data) => {
+    value = data;
+  });
+
+  return value;
+}
+
+export { Alert, AlertCall };
