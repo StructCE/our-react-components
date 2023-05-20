7d6c9deea1c75e14a1a626c25b0a08627d13b3e8
jessica8melo <jessica8melo@gmail.com>
2023-05-20 13:11:41 -0300

    🎨 Modal structure updated

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 export const DialogOverlay = styled(DialogPrimitive.Overlay)`
   position: absolute;
+  background: radial-gradient(rgba(100, 100, 100, 0.4), rgba(0, 0, 0, 0.4));
   inset: 0;
   animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
   @keyframes overlayShow {
@@ -25,9 +26,6 @@ export const DialogContent = styled(DialogPrimitive.Content)`
   animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
   box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
     hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  :focus {
-    outline: none;
-  }
   @keyframes contentShow {
     from {
       opacity: 0;
