5e44e7bb672d66aef11d44e6b8f1b9a1f37ebb30
GuilhermeSams <guilhermeps3670@gmail.com>
2023-05-31 00:21:08 -0300

    Instalando Tailwind e configurando

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/components/Carousel/styles.tsx",
+    "./src/components/Carousel/example/index.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
